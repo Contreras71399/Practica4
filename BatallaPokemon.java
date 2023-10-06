c7937ae7d0b2df3edffce26a87609da7853a26f2
Contreras71399 <145361635+Contreras71399@users.noreply.github.com>
2023-10-06 09:22:10 -0600

    Update BatallaPokemon.java

--- BatallaPokemon.java
+++ BatallaPokemon.java
@@ -3,16 +3,21 @@ public class BatallaPokemon {
        PokemonTipo Pokemon1=new PokemonTipo("Gyarados",10,"Volador");
        PokemonTipo Pokemon2=new PokemonTipo("Jolteon",10,"Electrico");
        
-       realizaAtaque(Pokemon1,Pokemon2,"Hidrobomba");
-       realizaAtaque(Pokemon2,Pokemon1,"Impactrueno");
+       realizarAtaque(Pokemon1,Pokemon2,"Hidrobomba");
+       realizarAtaque(Pokemon2,Pokemon1,"Impactrueno");
        
-       System.out.println(Pokemon1.getNombre()+"tiene ahora"+Pokemon1.getHP()+"tiene puntos ");
+       System.out.println(Pokemon1.getNombre()+"tiene ahora"+Pokemon1.getHP()+"tiene puntos de vida ");
     }
 
-    private static void realizaAtaque(PokemonAtacante, PokemonObjetivo, String ataque) {
-        int  danio= CalcularDanio(ataque);
-        int PuntosDeVidaObjetivo=
-                
+    public static void realizarAtaque(Pokemon Ata, Pokemon obj, String ataque) {
+        int  danio= dano(ataque);
+        int PuntosVida=objetivo.getHP()
+            PuntosVida=danio;
+        objetivo.setHP(PuntosVida);
+        System.out.println(Ata.getNombre()+ "ataca a "+ obj.getNombre()+ "con" + ataque+ "y causa" + dano + "Puntos de daño");
+                public static int dano (String ataque){
+                    return 1;
+    }
                 }
                 }
 
